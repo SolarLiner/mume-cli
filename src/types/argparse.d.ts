27e91beb8504d87a7f0f88665403f814c86af3ff
Nathan Graule <solarliner@gmail.com>
2017-08-23 20:34:34 +0700

    Added gulpfile and TS config

---
+++ src/types/argparse.d.ts
@@ -0,0 +1,10 @@
+export class ArgumentParserOptions {
+    description: string;
+    argumentDefault: any;
+    prefixChars: string;
+    conflictHandler: string;
+    addHelp: boolean;
+    parents: Array<any>;
+    prog: string;
+    formatterClass: any;
+}
